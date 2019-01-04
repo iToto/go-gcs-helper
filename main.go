a3db44a511235cbdd8e377e78df86553cc7a49da
Salvatore D'Agostino <toto19@gmail.com>
2019-01-03 23:08:35 -0500

    add CLI

--- main.go
+++ main.go
@@ -16,6 +16,39 @@ const (
 )
 
 func main() {
+	var validCommands = [...]string{"snapshot"}
+
+	if len(os.Args) != 2 {
+		err := fmt.Errorf("Usage: go-gcs-helper [COMMAND]")
+		log.Printf(err.Error())
+		os.Exit(1)
+	}
+
+	cmd := os.Args[1]
+
+	switch cmd {
+	case "snapshot":
+		createSnapshot()
+	default:
+		err := fmt.Errorf(
+			"invalid command. List of valid commands: %v",
+			validCommands,
+		)
+		log.Printf(err.Error())
+		os.Exit(1)
+	}
+}
+
+func createSnapshot() {
+	// Validate ENV vars
+	if "" == os.Getenv(VOLUME) ||
+		"" == os.Getenv(PROJECT) ||
+		"" == os.Getenv(ZONE) ||
+		"" == os.Getenv(SNAPSHOT_BASE_NAME) {
+		fmt.Println("Missing Env Vars!")
+		os.Exit(1)
+	}
+
 	start := time.Now()
 	snapshotName := fmt.Sprintf(
 		"%s-%d",
